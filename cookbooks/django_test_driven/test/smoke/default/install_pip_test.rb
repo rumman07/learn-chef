4588b5f25b2a8da9d1c49040d9704dac165de111
rahmedsd <rumman.ahmed@walgreens.com>
2017-07-26 17:45:58 -0500

    made progress on django_test_drive at 50% now

--- cookbooks/django_test_driven/test/smoke/default/install_pip_test.rb
+++ cookbooks/django_test_driven/test/smoke/default/install_pip_test.rb
@@ -1,11 +1,11 @@
 # # encoding: utf-8
 
-# Inspec test for recipe django_test_driven::default
+# Inspec test for recipe django_test_driven::install_pip
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-# =beginunless os.windows?
+# unless os.windows?
 #   # This is an example test, replace with your own test.
 #   describe user('root'), :skip do
 #     it { should exist }
@@ -15,7 +15,7 @@
 # # This is an example test, replace it with your own test.
 # describe port(80), :skip do
 #   it { should_not be_listening }
-# #=end=end
+# end
 
 describe command ('django-admin --version') do
       its(:stdout) { should match(/1.11.3/) }
