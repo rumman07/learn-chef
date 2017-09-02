e6168a5e4d067a48404a346575fe35b8ac9c0968
rumman07 <rummanahmed.ra@gmail.com>
2017-09-02 09:06:16 +0000

    added how to create dynamic values in the template files

---
+++ linuxacademy_chef_devops/cookbooks/apache/attributes/default.rb
@@ -0,0 +1,2 @@
+default["apache"]["sites"]["rumman2"] = { "port" => 80, "domain" => "rumman2.mylabserver.com" }
+default["apache"]["sites"]["rumman2b"] = { "port" => 80, "domain" => "rumman2b.mylabserver.com" }
